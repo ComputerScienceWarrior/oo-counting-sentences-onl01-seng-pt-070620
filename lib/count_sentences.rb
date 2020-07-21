6b457e18789b5c911e882965ade9555ae22b16b9
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-21 18:49:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,17 +16,18 @@ class String
 
   def count_sentences
     sentence = self.split
-    binding.pry
-    
-    if sentence.exclamation?
+    # binding.pry
+    sentence.each do |words|
+      if sentence.exclamation?
       sentence = sentence.split('!')
     elsif sentence.question?
       sentence = sentence.split('?')
     elsif sentence.sentence?
       sentence = sentence.split('.')
     elsif sentence.size == 0
-      
     end
+      
+  
     return sentence.size
   end
 end

96eb959d5833bf2054284f91d2b4d0455b511abe
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-21 18:21:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    self.end_with?('.') ? true : false
   end
 
   def question?
-
+    self.end_with?('?') ? true : false
   end
 
   def exclamation?
-
+    self.end_with?('!') ? true : false
   end
 
   def count_sentences
-
+    sentence = self.split
+    binding.pry
+    if sentence.exclamation?
+      sentence = sentence.split('!')
+    elsif sentence.question?
+      sentence = sentence.split('?')
+    elsif sentence.sentence?
+      sentence = sentence.split('.')
+    elsif sentence.size == 0
+      
+    end
+    return sentence.size
   end
 end
